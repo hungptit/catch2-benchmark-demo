915dfa61b00c7a89acd862007b8ba324115509d0
Hung Dang <hungptit@gmail.com>
2021-02-10 16:54:14 -0500

    Add a BDD example

---
+++ unittests/bdd_test.cpp
@@ -0,0 +1,32 @@
+#define CATCH_CONFIG_MAIN
+#include "catch2/catch.hpp"
+
+#include <vector>
+
+SCENARIO( "vectors can be sized and resized", "[vector]" ) {
+
+    GIVEN( "A vector with some items" ) {
+        std::vector<int> v( 5 );
+
+        REQUIRE( v.size() == 5 );
+        REQUIRE( v.capacity() >= 5 );
+
+        WHEN( "the size is increased" ) {
+            v.resize( 10 );
+
+            THEN( "the size and capacity change" ) {
+                REQUIRE( v.size() == 12 );
+                REQUIRE( v.capacity() >= 10 );
+            }
+        }
+        
+        WHEN( "the size is reduced" ) {
+            v.resize( 0 );
+
+            THEN( "the size changes but not capacity" ) {
+                REQUIRE( v.size() == 0 );
+                REQUIRE( v.capacity() >= 5 );
+            }
+        }
+    }
+}
